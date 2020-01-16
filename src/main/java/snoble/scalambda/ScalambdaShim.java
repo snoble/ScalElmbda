5aff896456e67ac6f423101583bf464f26c88906
Steven Henry Klaiber Noble <steven.noble@gmail.com>
2020-01-16 15:32:56 -0800

    It actually runs now

--- src/main/java/snoble/scalambda/ScalambdaShim.java
+++ src/main/java/snoble/scalambda/ScalambdaShim.java
@@ -3,7 +3,7 @@ package snoble.scalambda;
 public class ScalambdaShim {
 
     static {
-        System.loadLibrary("Scalambda");
+        System.loadLibrary("go-scalambda");
     }
     public native void start();
     public native void writeResponse(String input);
