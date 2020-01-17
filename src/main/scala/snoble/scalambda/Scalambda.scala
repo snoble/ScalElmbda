bc49ddcfa825b3494616178e1b14b932f4f8fb6a
Steven Henry Klaiber Noble <steven.noble@gmail.com>
2020-01-16 20:11:42 -0800

    Put shim in scala

--- src/main/scala/snoble/scalambda/Scalambda.scala
+++ src/main/scala/snoble/scalambda/Scalambda.scala
@@ -1,9 +1,16 @@
 package snoble.scalambda
 
+class ScalambdaShim {
+  @native def start(): Unit
+  @native def writeResponse(input: String): Unit
+  @native def readRequest(): String
+}
+
 object Scalambda {
 
   def main(args: Array[String]): Unit = {
     println("Starting")
+    System.loadLibrary("go-scalambda")
     val sl = new ScalambdaShim()
     sl.start()
 
