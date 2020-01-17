dfe0a0b9d4394f55fb6f0b21bc12693dd71e118b
Steven Henry Klaiber Noble <steven.noble@gmail.com>
2020-01-16 20:07:09 -0800

    Better path to data file

--- src/main/java/snoble/scalambda/ScalambdaShim.java
+++ src/main/java/snoble/scalambda/ScalambdaShim.java
@@ -3,6 +3,8 @@ package snoble.scalambda;
 public class ScalambdaShim {
 
     static {
+        String javaLibPath = System.getProperty("java.library.path");
+	System.out.println(javaLibPath);
         System.loadLibrary("go-scalambda");
     }
     public native void start();
