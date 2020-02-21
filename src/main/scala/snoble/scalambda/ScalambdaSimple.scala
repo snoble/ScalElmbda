1794ee39504ef24e1774c758e2780ef417170063
Steven Henry Klaiber Noble <steven.noble@gmail.com>
2020-02-21 15:20:33 -0800

    Refactor

--- src/main/scala/snoble/scalambda/ScalambdaSimple.scala
+++ src/main/scala/snoble/scalambda/ScalambdaSimple.scala
@@ -4,25 +4,28 @@ import scalaj.http._
 import scala.annotation.tailrec
 import scala.util.Try
 import io.circe.syntax._, io.circe.{Encoder, Json, Decoder}, io.circe.parser.decode
+import scala.util.{Try, Success, Failure}
 
 import snoble.scalambda.proto.simple.{Request, Response}
 
 object ScalambdaSimple {
+  val base64Decoder = java.util.Base64.getDecoder()
+  val base64Encoder = java.util.Base64.getEncoder()
 
   case class AWSResponse(
-    body: String,
+    body: Response,
     headers: Map[String, String]
   )
   implicit val encodeResponse: Encoder[AWSResponse] = Encoder.instance[AWSResponse](r =>
     Json.obj(
-      "body" -> r.body.asJson,
+      "body" -> base64Encoder.encodeToString(r.body.toByteArray).asJson,
       "headers" -> r.headers.asJson,
       "isBase64Encoded" -> true.asJson
     )
   )
 
   case class AWSRequest(
-    body: String,
+    body: Request,
     httpMethod: String,
     resource: String,
     path: String,
@@ -31,54 +34,59 @@ object ScalambdaSimple {
   implicit val decodeRequest: Decoder[AWSRequest] = Decoder.instance[AWSRequest]( c =>
       for {
         body <- c.downField("body").as[String]
+        parsedBody = Request.parseFrom(base64Decoder.decode(body))
         httpMethod <- c.downField("httpMethod").as[String]
         resource <- c.downField("resource").as[String]
         path <- c.downField("path").as[String]
         isBase64Encoded <- c.downField("isBase64Encoded").as[Boolean]
-      } yield AWSRequest(body, httpMethod, resource, path, isBase64Encoded)
+      } yield AWSRequest(parsedBody, httpMethod, resource, path, isBase64Encoded)
   )
 
-  val base64Decoder = java.util.Base64.getDecoder()
-  val base64Encoder = java.util.Base64.getEncoder()
-
-  def main(args: Array[String]): Unit = {
-    println("Starting")
-    val runtimeApi = System.getenv("AWS_LAMBDA_RUNTIME_API")
-    
-    @tailrec
-    def handleRequest(): Unit = {
-      val request: HttpResponse[String] = Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/next").asString
-      val requestId = request.header("Lambda-Runtime-Aws-Request-Id")
-      val arn = request.header("Lambda-Runtime-Invoked-Function-Arn")
-      val traceId = request.header("Lambda-Runtime-Trace-Id")
+  def handleRequests(runtimeApi: String, handleRequest: AWSRequest => Try[AWSResponse]) = {
+    val apiRequest: HttpResponse[String] = Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/next").asString
+      val requestId = apiRequest.header("Lambda-Runtime-Aws-Request-Id")
+      val arn = apiRequest.header("Lambda-Runtime-Invoked-Function-Arn")
+      val traceId = apiRequest.header("Lambda-Runtime-Trace-Id")
       println(s"requestId: $requestId")
-      println(s"request body: ${request.body}")
+      println(s"request body: ${apiRequest.body}")
 
-      val headers = Map("Access-Control-Allow-Origin" -> "*", "Content-Type" -> "application/x-protobuf")
-      val response = for {
-        body <- decode[AWSRequest](request.body)
-        requestByteArray = base64Decoder.decode(body.body)
-        simpleRequest = Request.parseFrom(requestByteArray)
-        simpleByteArray = Response(simpleRequest.low, simpleRequest.high).toByteArray
-        simpleEncoded = base64Encoder.encodeToString(simpleByteArray)
-      } yield AWSResponse(simpleEncoded, headers).asJson.toString
-
-      println(s"debug: ${response}")
+      val response = decode[AWSRequest](apiRequest.body).map(handleRequest(_))
 
       (requestId, response) match {
-        case (Some(id), Right(r)) => Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/$id/response")
-          .postData(r)
+        case (Some(id), Right(Success(r))) => Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/$id/response")
+          .postData(r.asJson.toString)
           .asString
-        case (Some(id), Left(_)) => Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/$id/error")
+        case (Some(id), Right(Failure(e))) => Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/$id/error")
+          .postData(Map("errorMessage" -> "fail", "errorType" -> "BadFail").asJson.toString)
+          .asString
+        case (Some(id), Left(parseError)) => Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/$id/error")
           .postData(Map("errorMessage" -> "fail", "errorType" -> "BadFail").asJson.toString)
           .asString
         case (None, _) => Http(s"http://$runtimeApi/2018-06-01/runtime/invocation/init/error")
           .postData(Map("errorMessage" -> "no request id", "errorType" -> "MissingRequestIdHeader").asJson.toString)
           .asString
       }
-      handleRequest()
-    }
+  }
 
-    handleRequest()
+  def egRequestHandler(request: AWSRequest): Try[AWSResponse] = {
+    Try {
+      val headers = Map("Access-Control-Allow-Origin" -> "*", "Content-Type" -> "application/x-protobuf")
+      AWSResponse(
+        Response(request.body.low, request.body.high),
+        headers
+      )
+    }
+  }
+
+  def start(requestHandler: AWSRequest => Try[AWSResponse]) = {
+    println("Starting")
+    val runtimeApi = System.getenv("AWS_LAMBDA_RUNTIME_API")
+    while(true) {
+      handleRequests(runtimeApi, requestHandler)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    start(egRequestHandler(_))
   }
 }
